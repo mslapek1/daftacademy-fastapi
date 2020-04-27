555467b04ce3c0860b1bd9c25b794113d790db01
mslapek1 <mariusz.slapek@gmail.com>
2020-04-27 05:38:28 +0200

    add lec3ex5

--- main.py
+++ main.py
@@ -141,20 +141,16 @@ def all(session_token: str = Cookie(None)):
 def get(pt: int, session_token: str = Cookie(None)):
 	if session_token not in list(app.sessions.keys()):
 		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Error", headers={"WWW-Authenticate": "Basic"},)
-	if(pt > app.counter or pt < 1):
+	if(pt >= app.counter or pt < 1):
 		return HTTPException(status_code=204, detail="Error - wrong value")
 
-	return app.list[pt]
+	return app.list[pt - 1]
 
 @app.delete("/patient/{pt}")
 def del_patient(response: Response, pt: int, session_token: str = Cookie(None)):
 	if session_token not in list(app.sessions.keys()):
 		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Error", headers={"WWW-Authenticate": "Basic"},)	
-	if(pt > app.counter or pt < 1):
+	if(pt >= app.counter or pt < 1):
 		return HTTPException(status_code=204, detail="Error - wrong value")
 	app.counter -= 1
 	app.list.pop(pt)
-	response.status_code = 204
-	response.headers["Location"] = "/patient/{pk}"
-
-	return response
