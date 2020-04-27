da07257af0a39aa606e767ac8169166bd4490985
mslapek1 <mariusz.slapek@gmail.com>
2020-04-27 02:19:16 +0200

    lec3_ex2

--- main.py
+++ main.py
@@ -76,20 +76,16 @@ security = HTTPBasic()
 
 
 @app.post("/login")
-def login(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "trudnY")
-    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+def login(cred: HTTPBasicCredentials = Depends(security)):
+    username = secrets.compare_digest(cred.username, "trudnY")
+    password = secrets.compare_digest(cred.password, "PaC13Nt")
 
-    if not correct_username or not correct_password:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
+    if not username or not password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong data", headers={"WWW-Authenticate": "Basic"},)
 
-    session_token = sha256(
-        bytes(f"{credentials.username}{credentials.password}{app.secret_key}", "utf8")
-    ).hexdigest()
+    session_token = sha256(bytes(f"{cred.username}{cred.password}{app.secret_key}", "utf8")).hexdigest()
+    
     response: RedirectResponse = RedirectResponse("/welcome", 302)
     response.set_cookie(key="session_token", value=session_token)
+
     return response
